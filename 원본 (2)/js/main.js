d486336bcfd4157ec49090dbeb2910ea9210ce6d
ZziBooOooo <zziboo3773@gmail.com>
2022-12-02 07:20:21 +0900

    responsive

--- 원본 (2)/js/main.js
+++ 원본 (2)/js/main.js
@@ -1,4 +1,4 @@
-console.log(window.scrollY);
+/* console.log(window.scrollY); */
 let currentscroll = window.scrollY;
 
 const a = document.querySelector(".main_third_box_text2");
@@ -9,16 +9,15 @@ const d = document.querySelector(".Main5_text_1");
 const header = document.querySelector(".header_desktop");
 const menuBtn = document.querySelector(".menuBtn");
 
-console.log(a);
+/* console.log(a); */
 
 window.addEventListener("scroll", () => {
   /* if (currentscroll >= 666) {
     a.style.backgroundColor = "Tomato";
   } */
   let currentScrollValue = document.documentElement.scrollTop;
-  console.log(currentScrollValue);
+  /*   console.log(currentScrollValue);
   if (currentScrollValue >= 1550) {
-    /*  a.style.backgroundColor = "Tomato"; */
     a.classList.add("active");
   }
 
@@ -31,7 +30,7 @@ window.addEventListener("scroll", () => {
   }
   if (currentScrollValue >= 3350) {
     d.classList.add("active2");
-  }
+  } */
 
   /*   if (currentscroll >= 133) {
     menuBtn.style.position = "fixed";
