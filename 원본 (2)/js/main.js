410386ae766cb2ac291d3bd2d5136ad014b4044e
minsuh Jo <zziboo3773@gmail.com>
2022-12-06 00:41:16 +0900

    finish

--- 원본 (2)/js/main.js
+++ 원본 (2)/js/main.js
@@ -1,23 +1,27 @@
 /* console.log(window.scrollY); */
-let currentscroll = window.scrollY;
+
+/* 스크립트 해야할 것
+프로덕트, 파인드어필 페이지에서 화살표누르면 해당 컨텐츠로 이동,
+헤더 없어지면 메뉴버튼 나오게
+// */
+// let currentscroll = window.scrollY;
 
 const a = document.querySelector(".main_third_box_text1");
 const b = document.querySelector(".main_third_box_text2");
 const allMain3 = document.querySelector(".main_third");
 const c = document.querySelector(".main6_textbox");
-const allMain6 = document.querySelector('.main_6')
-const d = document.querySelector('.Main5_text_1');
-console.log(d)
-const e = document.querySelector('.Main5_text_2');
-const allMain5 = document.querySelector('.Main_5');
+const allMain6 = document.querySelector(".main_6");
+const d = document.querySelector(".Main5_text_1");
+console.log(d);
+const e = document.querySelector(".Main5_text_2");
+const allMain5 = document.querySelector(".Main_5");
 // let ah = a.offsetHeight;
 // let bh = b.offsetHeight;
 const allh3 = allMain3.offsetHeight;
 const allh6 = allMain6.offsetHeight;
 const allh5 = allMain5.offsetHeight;
 
-
-console.log(b);
+// console.log(b);
 
 /* window.addEventListener("scroll", function () {
   console.log(b);
@@ -28,24 +32,26 @@ console.log(b);
 $(window).scroll(function () {
   var scrollHeight = $(document).scrollTop();
 
-  if ( scrollHeight >= $(a).offset().top-allh3) {
+  if (scrollHeight >= $(a).offset().top - allh3) {
     // console.log("여기");
     a.classList.add("active");
   }
 
-  if ( scrollHeight >= $(b).offset().top-allh3) {
+  if (scrollHeight >= $(b).offset().top - allh3) {
     b.classList.add("active");
   }
 
-  if(scrollHeight >= $(c).offset().top-allh6*1.5) {
-    c.classList.add('active')
+  if (scrollHeight >= $(c).offset().top - allh6 * 1.5) {
+    c.classList.add("active");
   }
 
-  if(scrollHeight >= $(d).offset().top-allh5)
-  d.classList.add('active2')
+  if (scrollHeight >= $(d).offset().top - allh5) {
+    d.classList.add("active2");
+  }
 
-  if(scrollHeight >= $(e).offset().top-allh5)
-  e.classList.add('active2')
+  if (scrollHeight >= $(e).offset().top - allh5) {
+    e.classList.add("active2");
+  }
 });
 
 // window.addEventListener("scroll", () => {
---
+++ 원본 (2)/js/menu_button.js
@@ -0,0 +1,21 @@
+const headerHeight = document.querySelector("header");
+const menuBtn = document.querySelector(".menuBtn");
+// console.log(headerHeight.offsetHeight)
+
+// let a = headerHeight.offsetHeight;
+
+$(window).scroll(() => {
+  //   console.log(headerHeight.offsetHeight);
+  //   console.log($(document).scrollTop());
+
+  if (a <= $(document).scrollTop()) {
+    console.log("hi");
+    // alert('d')
+    console.log(menuBtn);
+    menuBtn.classList.add("menuBtn_on");
+    menuBtn.style = `opacity:1;`;
+  } else {
+    menuBtn.style = `opacity:0;`;
+  }
+});
+// css에서 스타일하면 적용이 안됨 ->
