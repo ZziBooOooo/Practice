a4ec40098f3cc65299d1f3717c3b289304043ec3
ZziBooOooo <zziboo3773@gmail.com>
2022-12-10 15:24:46 +0900

    folder

---
+++ 클론/Apeel/js/main.js
@@ -0,0 +1,78 @@
+/* console.log(window.scrollY); */
+
+const a = document.querySelector(".main_third_box_text2");
+const b = document.querySelector(".main_third_box_text1");
+const allMain3 = document.querySelector(".main_third");
+const c = document.querySelector(".main6_textbox");
+const allMain6 = document.querySelector(".main_6");
+const d = document.querySelector(".Main5_text_1");
+console.log(d);
+const e = document.querySelector(".Main5_text_2");
+const allMain5 = document.querySelector(".Main_5");
+// let ah = a.offsetHeight;
+// let bh = b.offsetHeight;
+const allh3 = allMain3.offsetHeight;
+const allh6 = allMain6.offsetHeight;
+const allh5 = allMain5.offsetHeight;
+
+console.log(b);
+
+/* window.addEventListener("scroll", function () {
+  console.log(b);
+  console.log($(b).offset().top);
+  console.log($(b).scrollTop());
+}); */
+
+$(window).scroll(function () {
+  var height = $(document).scrollTop();
+  console.log(height);
+  console.log($(b).offset().top);
+
+  console.log($(allMain3).offset().top);
+
+  if (height >= $(a).offset().top - height) {
+    // console.log("여기");
+    a.classList.add("active");
+  }
+
+  if (height >= $(b).offset().top) {
+    console.log("여기");
+    b.classList.add("active");
+  }
+});
+
+// window.addEventListener("scroll", () => {
+/* if (currentscroll >= 666) {
+    a.style.backgroundColor = "Tomato";
+  } */
+// let currentScrollValue = document.documentElement.scrollTop;
+/*   console.log(currentScrollValue);
+  if (currentScrollValue >= 1550) {
+    a.classList.add("active");
+  }
+
+  if (currentScrollValue >= 1150) {
+    b.classList.add("active");
+  }
+
+  if (currentScrollValue >= 4340) {
+    c.classList.add("active");
+  }
+  if (currentScrollValue >= 3350) {
+    d.classList.add("active2");
+  } */
+
+/*   if (currentscroll >= 133) {
+    menuBtn.style.position = "fixed";
+    menuBtn.style.display = "block";
+  } */
+// });
+
+/*
+컨텐츠만큼 위에서 떨어진 정도와 (ottsetTop)
+현재 스크롤의 위치를 비교했을 때 (scrollTop)
+이벤트? 클래스 추가
+위처럼 숫자를 직접 대입 X
+
+
+*/
