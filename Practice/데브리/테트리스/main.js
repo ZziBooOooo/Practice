420fe6407b148b722c007142296cf7042c9e6751
ZziBooOooo <zziboo3773@gmail.com>
2022-12-21 17:55:15 +0900

    tetris2

--- Practice/데브리/테트리스/main.js
+++ Practice/데브리/테트리스/main.js
@@ -19,9 +19,24 @@ const BLOCKS = {
       [1, 0],
       [1, 1],
     ],
-    [],
-    [],
-    [],
+    [
+      [1, 2],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [1, 2],
+      [0, 1],
+      [2, 1],
+      [1, 1],
+    ],
+    [
+      [2, 1],
+      [1, 2],
+      [1, 0],
+      [1, 1],
+    ],
   ],
 };
 const movingItem = {
@@ -36,7 +51,8 @@ init();
 // Functions
 
 function init() {
-  // spread Operator : movingItem의 값만 가져오기 때문에 temp의값은 변경되지 않는다.
+  // spread Operator : movingItem의 값만 가져온다.
+  // tempMovingItem의 값은 바뀌는데 init하면 초기값을 가져오도록 하려고 여기에 위치시킴
   tempMovingItem = { ...movingItem };
 
   for (let i = 0; i < GAME_ROWS; i++) {
@@ -56,29 +72,85 @@ function prependNewLine() {
   playground.prepend(li);
 }
 
-function renderBlocks() {
+// moveType 보내지않을거라서 빈값으로 초기화
+function renderBlocks(moveType = "") {
   const { type, direction, top, left } = tempMovingItem; // destructuring
   // console.log(tempMovingItem.type); // 이런식으로 접근하기 싫어서 디스트럭쳐링 씀
   // console.log(type, direction, top, left);
   // console.log(BLOCKS[type][direction]);
   // console.log(BLOCKS[type]);
   const movingBlocks = document.querySelectorAll(".moving");
+  // console.log(movingBlocks);
   movingBlocks.forEach((moving) => {
     moving.classList.remove(type, "moving");
+    // moving 붙으면 지우고 아래에서 다시 타겟에만 클래스 추가
     // console.log(moving);
   });
   BLOCKS[type][direction].forEach((block) => {
     // console.log(block);
-    const x = block[0] + left; // 중앙에서 시작하도록 left(3)만큼 더한다.
-    const y = block[1] + top;
-    const target = playground.childNodes[y].childNodes[0].childNodes[x];
-    console.log(target);
-    target.classList.add(type, "moving");
+    const x = block[0] + left; // 배열의 첫번째값-x좌표 / 중앙에서 시작하도록 left(3)만큼 더한다.
+    const y = block[1] + top; //
+    // 삼항연산자는 변수에도 담을 수 있다.
+
+    // const target = playground.childNodes[y].childNodes[0].childNodes[x];
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null; // 있으면 값을 담고 없으면 null을 담는다.
+    // console.log(target);
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving"); // type은 tree이고 css에서 .tree클래스를 작성했다.
+      // 키보드이벤트-> moveBlock -> renderBlock ->해서 나온 target에는 moving클래스를 추가
+    } else {
+      tempMovingItem = { ...movingItem };
+      // 원상복구 한 뒤 렌더블록
+      setTimeout(() => {
+        renderBlocks(); // 재귀함수 ! - 종료조건을 setTimeOut으로 설정
+        if (moveType === "top") {
+          seizeBlock();
+        }
+      }, 0);
+    }
   });
+
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
+  // 없으면 원상태로 돌아간다. 위에 tempMovingItem = { ...movingItem };때문
+  // tempMovingItem.left=left 하면 그냥 공간을 넘어간다 - 자기자신 값을 넣는거니까 당연,,
+  // movingItem의 값은 왜 바뀌는걸까.?
+}
+function seizeBlock() {
+  console.log("seize");
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove("moving");
+    moving.classList.add("seized");
+  });
+}
+
+function checkEmpty(target) {
+  if (!target) {
+    return false;
+  }
+  return true;
 }
 
 function moveBlock(moveType, amount) {
   tempMovingItem[moveType] += amount;
+  renderBlocks(moveType);
+}
+
+function changeDirection() {
+  /* tempMovingItem.direction += 1;
+  if (tempMovingItem.direction === 3) {
+    tempMovingItem.direction = 0;
+  } */
+
+  const direction = tempMovingItem.direction;
+  direction === 3
+    ? (tempMovingItem.direction = 0)
+    : (tempMovingItem.direction += 1);
   renderBlocks();
 }
 // event handling
@@ -93,6 +165,9 @@ document.addEventListener("keydown", (e) => {
       break;
     case 40:
       moveBlock("top", 1);
+      break;
+    case 38:
+      changeDirection();
       break;
     default:
       break;
--- Practice/데브리/테트리스/test.js
+++ Practice/데브리/테트리스/test.js
@@ -19,11 +19,18 @@ const movingItem2 = {
   left2: 3,
 };
 
-const { type2, direction2, top2, left2 } = movingItem2;
+/* const { type2, direction2, top2, left2 } = movingItem2;
 console.log(type2); // tree
 console.log(direction2); //0
 console.log(BLOCKS2); // {tree:Array(4)}
 console.log(BLOCKS2[type2]); // 객체[키] => 객체의 키에 해당하는 값
-// tree객체를 불렀지만 인덱스가 없어서 첫번째요소가 나온 것!
+//BLOCKS[type2]는 tree배열을 부른것
 console.log(BLOCKS2[type2][direction2]);
 // [direction2]는 0이라서 tree객체의 첫번째값이 나온것
+ */
+
+let tempMovingItem2 = { ...movingItem2 };
+
+tempMovingItem2.left2 = 5;
+console.log(tempMovingItem2);
+console.log(movingItem2);
